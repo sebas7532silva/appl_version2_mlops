69bab2a2449a8056168d23bf03a0efb91685ace0
sebas7532silva <sebas7532silva>
2025-06-11 23:23:26 +0000

    changin fetcher

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -3,9 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 def get_stock_data(ticker="AAPL", days=30):
-    end = datetime.today()
-    start = end - timedelta(days=days)
-    df = yf.download(ticker, start=start, end=end)
+    df = yf.download(ticker, period="30d", interval="1h", auto_adjust=False)
     df.dropna(inplace=True)
     df['Return'] = df['Close'].pct_change().fillna(0)
     return df
