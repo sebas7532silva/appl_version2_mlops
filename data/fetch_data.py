0cdf9c8f7ff8e48884277677b19f930a681b4351
sebas7532silva <sebas7532silva>
2025-06-11 23:25:37 +0000

    changing to google ticker

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -2,8 +2,8 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
 
-def get_stock_data(ticker="AAPL", days=30):
-    df = yf.download(ticker, period="30d", interval="1h", auto_adjust=False)
+def get_stock_data(ticker="GOOG", days=30):
+    df = yf.download(ticker, period="7d", interval="1h", auto_adjust=False)
     df.dropna(inplace=True)
     df['Return'] = df['Close'].pct_change().fillna(0)
     return df
