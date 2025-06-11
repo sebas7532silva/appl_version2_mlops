650f212bba0e4fb1cdde2cf11cea1b81cb479621
sebas7532silva <sebas7532silva>
2025-06-11 23:08:27 +0000

    modifying fetch data name function

--- tests/test_fetch_data.py
+++ tests/test_fetch_data.py
@@ -1,7 +1,7 @@
-from data.fetch_data import fetch_stock_data
+from data.fetch_data import fetch_data
 
-def test_fetch_stock_data():
-    df = fetch_stock_data()
+def test_fetch_data():
+    df = fetch_data()
     assert not df.empty
     assert "Close" in df.columns
     assert "Volume" in df.columns
